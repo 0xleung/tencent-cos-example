f8f2b6766c7120c3e4f2aacc417a192d00dabc92
0xleung <jobinleung@gmail.com>
2022-03-28 14:36:19 +0800

    chore: env

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.18
 require (
 	github.com/clbanning/mxj v1.8.4 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
 	github.com/mitchellh/mapstructure v1.4.3 // indirect
 	github.com/mozillazg/go-httpheader v0.3.0 // indirect
 	github.com/tencentyun/cos-go-sdk-v5 v0.7.34 // indirect
--- src/main.go
+++ src/main.go
@@ -2,30 +2,46 @@ package main
 
 import (
 	"context"
+	"log"
 	"net/http"
 	"net/url"
+	"os"
 	"strings"
 
+	"github.com/joho/godotenv"
 	"github.com/tencentyun/cos-go-sdk-v5"
 )
 
 func main() {
-	u, _ := url.Parse("https://0xleung-1257283327.cos.ap-singapore.myqcloud.com")
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	bucketUrl := os.Getenv("BUCKET_URL")
+
+	secretID := os.Getenv("SECRETID")
+	secretKey := os.Getenv("SECRETKEY")
+
+	u, err := url.Parse(bucketUrl)
+	if err != nil {
+		panic(err)
+	}
 	b := &cos.BaseURL{BucketURL: u}
 	c := cos.NewClient(b, &http.Client{
 		Transport: &cos.AuthorizationTransport{
-			SecretID:  "AKIDnPlqeIfla9RTEjwsXw3tSjkTv61e16vT", // 替换为用户的 SecretId，请登录访问管理控制台进行查看和管理，https://console.cloud.tencent.com/cam/capi
-			SecretKey: "JL6aokPowmLegRv85l9G7h25sOdaGgph",     // 替换为用户的 SecretKey，请登录访问管理控制台进行查看和管理，https://console.cloud.tencent.com/cam/capi
+			SecretID:  secretID,  // 替换为用户的 SecretId，请登录访问管理控制台进行查看和管理，https://console.cloud.tencent.com/cam/capi
+			SecretKey: secretKey, // 替换为用户的 SecretKey，请登录访问管理控制台进行查看和管理，https://console.cloud.tencent.com/cam/capi
 		},
 	})
 	// 对象键（Key）是对象在存储桶中的唯一标识。
 	// 例如，在对象的访问域名 `examplebucket-1250000000.cos.COS_REGION.myqcloud.com/test/objectPut.go` 中，对象键为 test/objectPut.go
 	name := "test/objectPut.go"
 	// 1.通过字符串上传对象
-	f := strings.NewReader("test")
+	f := strings.NewReader("test1")
 
-	_, err := c.Object.Put(context.Background(), name, f, nil)
-	if err != nil {
+	_, errr := c.Object.Put(context.Background(), name, f, nil)
+	if errr != nil {
 		panic(err)
 	}
 	// // 2.通过本地文件上传对象
